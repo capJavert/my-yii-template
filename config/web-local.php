e526b3b80bacc086b5db3b1f99ffb167cf39bd6a
capJavert <ante.baric3@gmail.com>
2016-03-16 01:01:41 +0100

    web-local added lessc module

--- config/web-local.php
+++ config/web-local.php
@@ -38,24 +38,31 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        'assetManager' => [
+            'converter' => [
+                'class' => 'cakebake\lessphp\AssetConverter',
+                'compress' => true, // Optional: You can tell less.php to remove comments and whitespace to generate minimized css files.
+                'useCache' => true, // Optional: less.php will save serialized parser data for each .less file. Faster, but more memory-intense.
+                //'cacheDir' => null, // Optional: is passed to the SetCacheDir() method. By default "cakebake\lessphp\runtime" is used.
+                //'cacheSuffix' => true, // Optional: Filename suffix to avoid the browser cache and force recompiling by configuration changes
+            ],
+        ],
     ],
     'params' => $params,
 ];
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    /*$config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-    ];
+    ];*/
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
