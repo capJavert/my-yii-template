371d8e4cb14ae6978ab0ad7968c008ed6ea7ee65
capJavert <ante.baric3@gmail.com>
2016-04-10 07:40:21 +0200

    ff fix

--- web/js/main.js
+++ web/js/main.js
@@ -3,17 +3,17 @@ $( 'select[name=ispostava]' ).change(function() {
     var res = url.split("&");
     console.log(res);
     var gor= res[0]+"&ispostaveID="+$(this).val();
-    window.location.href = gor;
+    window.location = gor;
 });
 $( 'select[name=tim]' ).change(function() {
     var url      = window.location.href;
     var res = url.split("&");
     var gor= res[0]+"&timID="+$(this).val();
-    window.location.href = gor;
+    window.location = gor;
 });
 $( 'select[name=smjena]' ).change(function() {
     var url      = window.location.href;
     var res = url.split("&");
     var gor= res[0]+"&smjenaID="+$(this).val();
-    window.location.href = gor;
+    window.location = gor;
 });
