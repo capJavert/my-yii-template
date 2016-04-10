8b3e285cf8adce667026d8fa14b474fe1d84d70e
capJavert <ante.baric3@gmail.com>
2016-04-10 07:58:47 +0200

    get parameter

--- views/site/schedule.php
+++ views/site/schedule.php
@@ -3,6 +3,11 @@
 /* @var $this yii\web\View */
 
 $this->title = 'Raspored';
+
+if(isset($_GET['brojDana']))
+    $brojDana = $_GET['brojDana'];
+else
+    $brojDana = 0;
 ?>
 <div class="site-index">
     <div class="container" id="scheduler">
@@ -10,12 +15,14 @@ $this->title = 'Raspored';
             <h2>Raspored, kreiran datuma 09.09.2016</h2>
         </div>
 
-        <?php for($i=1;$i<28;$i++) {?>
+        <?php if($brojDana) { ?>
+        <?php for($i=1;$i<=$brojDana;$i++) {?>
         <div data-id="<?= 'box'.($i-1) ?>" class="col-md-2 box-container box-small">
             <a href="./scheduledetail/?day=<?= $i ?>"><div class="day">
                 Dan <?= $i ?>
             </div></a>
         </div>
         <?php } ?>
+        <?php } else echo '<div class="col-md-12"><h3>Nije definiran vremenski raspon..</h3></div>' ?>
     </div>
 </div>
