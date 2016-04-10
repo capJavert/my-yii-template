21d16c43f2dc5270a98f324cffe62db99bd8ae50
capJavert <ante.baric3@gmail.com>
2016-04-10 07:32:43 +0200

    /testApi shell script

---
+++ testApi/api.sh
@@ -0,0 +1,8 @@
+#wget -qO- http://capjavert.dev/logic/parse/ &> /dev/null
+#wget -qO- http://capjavert.dev/logic/assign/ &> /dev/null
+#wget http://capjavert.dev/logic/output/
+
+wget -qO- http://codetroopers.tk:2016/logic/parse/ &> /dev/null
+wget -qO- http://codetroopers.tk:2016/logic/assign/ &> /dev/null
+wget http://codetroopers.tk:2016/logic/output/
+
